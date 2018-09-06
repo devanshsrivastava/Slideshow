6d2c3380d54e27c472203107906ad18a88ea07b4
technogeek1 <32399386+technogeek1@users.noreply.github.com>
2018-09-06 13:21:19 +0530

    js

---
+++ slide.js
@@ -0,0 +1,22 @@
+var i=0;
+var images =[];
+var time = 1500;
+
+images[0] = '6.jpg';
+images[1] = '4.jpg';
+images[2] = '3.jpg';
+images[3] = '2.jpg';
+images[4] = '5.jpg';
+
+
+function changeImg() {
+	document.slide.src = images[i];
+
+	if (i<images.length-1) {
+		i++;
+	}else{
+		i=0;
+	}
+	setTimeout("changeImg()",time);
+}
+window.onload = changeImg;
